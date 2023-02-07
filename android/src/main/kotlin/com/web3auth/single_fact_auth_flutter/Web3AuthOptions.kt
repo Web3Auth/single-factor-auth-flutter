fba06fec0b1ab5221ead98aa74788e2bfd205a32
Gaurav Goel <grvgoel19@gmail.com>
2023-02-07 18:27:32 +0800

    Adding code for aggregate verifier in android

--- android/src/main/kotlin/com/web3auth/single_fact_auth_flutter/Web3AuthOptions.kt
+++ android/src/main/kotlin/com/web3auth/single_fact_auth_flutter/Web3AuthOptions.kt
@@ -3,4 +3,5 @@ package com.web3auth.single_fact_auth_flutter
 data class Web3AuthOptions(val network: String,
                            val verifier: String,
                            val email: String,
-                           val idToken: String)
+                           val idToken: String,
+                           val aggregateVerifier: String? = null)
