64c7256900a27d58f3ec7714411232ac4b996956
Musab Hussain <musabhussain36@gmail.com>
2023-04-13 13:21:13 +0500

    getNetwork func

--- ios/Classes/SingleFactAuthFlutterPlugin.swift
+++ ios/Classes/SingleFactAuthFlutterPlugin.swift
@@ -9,6 +9,24 @@ public class SingleFactAuthFlutterPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  private func getNetwork(_ network: String) -> TorusNetwork {
+    switch network {
+      case "mainnet":
+          return TorusNetwork.MAINNET
+      case "testnet":
+          return TorusNetwork.TESTNET
+      case "aqua":
+          return TorusNetwork.AQUA
+      case "cyan":
+          return TorusNetwork.CYAN
+      case "celeste":
+          return TorusNetwork.CELESTE
+      default:
+          return TorusNetwork.MAINNET
+      }
+  }
+
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
       case "getPlatformVersion":
@@ -16,7 +34,7 @@ public class SingleFactAuthFlutterPlugin: NSObject, FlutterPlugin {
         break
       case "getTorusKey":
         let args = call.arguments as! [String]
-        let singleFactorAuthArgs = SingleFactorAuthArgs(network: args[0])
+        let singleFactorAuthArgs = SingleFactorAuthArgs(network: self.getNetwork(args[0]))
         let singleFactorAuth = SingleFactorAuth(singleFactorAuthArgs: singleFactorAuthArgs)
         let loginParams = LoginParams(verifier: args[1], verifierId: args[2],  idToken: args[3])
         async {
@@ -30,7 +48,7 @@ public class SingleFactAuthFlutterPlugin: NSObject, FlutterPlugin {
         break
       case "getAggregateTorusKey":
         let args = call.arguments as! [String]
-        let singleFactorAuthArgs = SingleFactorAuthArgs(network: args[0])
+        let singleFactorAuthArgs = SingleFactorAuthArgs(network: self.getNetwork(args[0]))
         let singleFactorAuth = SingleFactorAuth(singleFactorAuthArgs: singleFactorAuthArgs)
         let loginParams = LoginParams(verifier: args[1], verifierId: args[2],  idToken: args[3], subVerifierInfoArray: [TorusSubVerifierInfo(verifier: args[1], idToken: args[3])])
         async {
