d8168a1f224499ac23eafd1ec90ce599106bd5ca
Musab Hussain <musabhussain36@gmail.com>
2023-04-13 13:16:14 +0500

    sfa ios

--- ios/Classes/SingleFactAuthFlutterPlugin.swift
+++ ios/Classes/SingleFactAuthFlutterPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import SingleFactorAuth
 
 public class SingleFactAuthFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +10,40 @@ public class SingleFactAuthFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+      case "getPlatformVersion":
+        result("iOS " + UIDevice.current.systemVersion)
+        break
+      case "getTorusKey":
+        let args = call.arguments as! [String]
+        let singleFactorAuthArgs = SingleFactorAuthArgs(network: args[0])
+        let singleFactorAuth = SingleFactorAuth(singleFactorAuthArgs: singleFactorAuthArgs)
+        let loginParams = LoginParams(verifier: args[1], verifierId: args[2],  idToken: args[3])
+        async {
+          do {
+            let torusKeyCF = try await singleFactorAuth.getKey(loginParams: loginParams)
+            result(torusKeyCF.map { $0.toJson() })
+          } catch {
+            result(FlutterError(code: "key_not_generated", message: "Key not generated", details: nil))
+          }
+        }
+        break
+      case "getAggregateTorusKey":
+        let args = call.arguments as! [String]
+        let singleFactorAuthArgs = SingleFactorAuthArgs(network: args[0])
+        let singleFactorAuth = SingleFactorAuth(singleFactorAuthArgs: singleFactorAuthArgs)
+        let loginParams = LoginParams(verifier: args[1], verifierId: args[2],  idToken: args[3], subVerifierInfoArray: [TorusSubVerifierInfo(verifier: args[1], idToken: args[3])])
+        async {
+          do {
+            let torusKeyCF = try await singleFactorAuth.getKey(loginParams: loginParams)
+            result(torusKeyCF.map { $0.toJson() })
+          } catch {
+            result(FlutterError(code: "key_not_generated", message: "Key not generated", details: nil))
+          }
+        }
+        break
+      default:
+        break
+    }
   }
 }
